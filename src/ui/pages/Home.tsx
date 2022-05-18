97e203683b4f0d335b1b666e0eeff47a038bd8dd
ED <eddie@ftwcoin.io>
2022-05-17 22:29:31 -0700

    Update

--- src/ui/pages/Home.tsx
+++ src/ui/pages/Home.tsx
@@ -1,7 +1,12 @@
 import React from "react";
 import PageLayout from "../components/PageLayout";
 import BgContainer from "../components/BgContainer";
-import {GALLERY_PATH, SMITH_PATH, SWAP_PATH, TOURNAMENT_PATH} from "../../consts";
+import {
+  GALLERY_PATH,
+  SMITH_PATH,
+  SWAP_PATH,
+  TOURNAMENT_PATH,
+} from "../../consts";
 import { ARENA_PATH } from "./Tournament/pageRoutes";
 import { Link } from "react-router-dom";
 import { useWallet } from "../../packages/provider";
@@ -9,13 +14,20 @@ import { TESTNET } from "../../packages/neo/consts";
 import toast from "react-hot-toast";
 
 const CARDS = [
-	{
-		title: "FTW Smith",
-		type: "Utility",
-		desc: "FTW Smith helps users to create and deploy fungible/Non-fungible token smart contracts without any codes.",
-		img: "assets/smith.png",
-		link: SMITH_PATH,
-	},
+  {
+    title: "FTW Swap",
+    type: "DeFi",
+    desc: "Coming soon. Try on our Testnet.",
+    img: "assets/swap1.png",
+    link: SWAP_PATH,
+  },
+  {
+    title: "FTW Smith",
+    type: "Utility",
+    desc: "FTW Smith helps users to create and deploy fungible/Non-fungible token smart contracts without any codes.",
+    img: "assets/smith.png",
+    link: SMITH_PATH,
+  },
   {
     title: "FTW Rune",
     type: "NFT",
@@ -29,13 +41,6 @@ const CARDS = [
     desc: " FTW Runes against each other with the victor earning a GAS prize.",
     img: "assets/arena-bg.jpeg",
     link: TOURNAMENT_PATH,
-  },
-  {
-    title: "FTW Swap",
-    type: "DeFi",
-    desc: "Coming soon. Try on our Testnet.",
-    img: "assets/swap.png",
-    link: SWAP_PATH,
   },
   // {
   //   title: "FTW Lab",
@@ -101,34 +106,6 @@ const Home = () => {
               </div>
             );
           })}
-          <div className="column is-3">
-            <div className="card">
-              <div
-                className="card-image is-clickable"
-                onClick={onSwitchNetwork}
-              >
-                <figure className="image">
-                  <BgContainer src={"assets/lab.png"} height="300px" />
-                </figure>
-              </div>
-              <div className="card-content">
-                <div className="media">
-                  <div className="media-content">
-                    <p
-                      onClick={onSwitchNetwork}
-                      className="title is-4 is-clickable"
-                    >
-                      FTW Lab
-                    </p>
-                    <span className="tag is-primary">Testnet</span>
-                  </div>
-                </div>
-                <div className="content" style={{ minHeight: "100px" }}>
-                  Preview FTW future apps.
-                </div>
-              </div>
-            </div>
-          </div>
         </div>
       </PageLayout>
     </div>
