0b94325c12eb0df832e09404409abb61bd69d932
ED <eddie@ftwcoin.io>
2022-06-12 21:17:55 -0700

    Add admin button

---
+++ src/ui/pages/Tournament/components/AdminLeaveButton.tsx
@@ -0,0 +1,38 @@
+import React from "react";
+import { useWallet } from "../../../../packages/provider";
+import { TournamentContract } from "../../../../packages/neo/contracts/ftw/tournament";
+import toast from "react-hot-toast";
+
+interface ILeaveButtonProps {
+  arenaNo: string;
+  tokenId: string;
+  setTxid: (txid: string) => void;
+}
+const LeaveButton = ({ arenaNo, tokenId, setTxid }: ILeaveButtonProps) => {
+  const { connectedWallet, network, addPendingTransaction } = useWallet();
+  const onLeave = async () => {
+    if (connectedWallet) {
+      try {
+        const res = await new TournamentContract(network).remove(
+          connectedWallet,
+          tokenId,
+          arenaNo
+        );
+        addPendingTransaction(res);
+        setTxid(res);
+      } catch (e: any) {
+        toast.error(e.message);
+      }
+    } else {
+      toast.error("Please connect wallet.");
+    }
+  };
+
+  return (
+    <button onClick={onLeave} className="button is-danger is-fullwidth">
+      Admin Kick
+    </button>
+  );
+};
+
+export default LeaveButton;
