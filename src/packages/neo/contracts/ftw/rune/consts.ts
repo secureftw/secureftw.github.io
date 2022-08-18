1f1c3bab6875d1bc36f98ede773c3aab1b358c85
Ediopia <hieddiejung@gmail.com>
2022-08-17 21:38:40 -0700

    Rune testnet hash change

--- src/packages/neo/contracts/ftw/rune/consts.ts
+++ src/packages/neo/contracts/ftw/rune/consts.ts
@@ -2,7 +2,7 @@ import { CONST } from "../../../index";
 
 export const RUNE_SCRIPT_HASH = {
   [CONST.PRIVATENET]: "3863225be4e0e3ea1874a32842fab0a3dc2fbfe3",
-  [CONST.TESTNET]: "3863225be4e0e3ea1874a32842fab0a3dc2fbfe3",
+  [CONST.TESTNET]: "9f30d01b85887d0927c01640d5789d908e7c315a",
   [CONST.MAINNET]: "bebd4eb7c09ca5b59004aa8b58c9bfc81270e5d6",
 };
 
