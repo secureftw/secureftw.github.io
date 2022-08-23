0092202e1cd9606daf7d90f43f0544c9167e09ea
Ediopia <hieddiejung@gmail.com>
2022-08-22 17:39:15 -0700

    Update

--- src/packages/neo/contracts/ftw/smith/consts.ts
+++ src/packages/neo/contracts/ftw/smith/consts.ts
@@ -2,7 +2,7 @@ import { CONST } from "../../../index";
 
 export const SMITH_SCRIPT_HASH = {
   [CONST.PRIVATENET]: "f161ae138e0255b809612e4d26e540c94c83c0cc",
-  [CONST.TESTNET]: "799704f9d8588b183204e154f8eea1de35bb3ecd",
+  [CONST.TESTNET]: "adafc1daf7d3f3d9626c8cc3c24d1b94da0c295a",
   [CONST.MAINNET]: "f860bbdcc5091a9ae7812b047fd325dfa7905ee1",
 };
 
--- src/packages/ui/AfterTransactionSubmitted/index.tsx
+++ src/packages/ui/AfterTransactionSubmitted/index.tsx
@@ -73,7 +73,7 @@ const AfterTransactionSubmitted = ({
               <a
                 target="_blank"
                 href={`https://${
-                  network === TESTNET ? "testnet." : ""
+                  network === TESTNET ? "testmagnet." : ""
                 }explorer.onegate.space/transactionInfo/${txid}`}
               >
                 View txid on explorer
